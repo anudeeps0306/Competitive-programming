177b45fd2afbbee19219e2388051470ec5398826
Anudeepexperiment <anudeep0306@gmail.com>
2023-10-01 10:47:52 +0530

    daily dose

--- Codeforces/B_Aleksa_and_Stack.cpp
+++ Codeforces/B_Aleksa_and_Stack.cpp
@@ -1,192 +1,63 @@
-#include <bits/stdc++.h>
-using namespace std;
-#define fastIO                             \
-    {                                      \
-        ios_base ::sync_with_stdio(false); \
-        cin.tie(NULL);                     \
-        cout.tie(NULL);                    \
-    }
-#define mod 1000000007
-#define PI 3.1415926535897932384626
-#define endl "\n"
-#define YES cout << "YES" \
-                 << "\n"
-#define NO cout << "NO" \
-                << "\n"
-#define ispowoftwo(n) (!(n & (n - 1)))
-#define pb push_back
-#define ppb pop_back
-#define int long long
+class Solution {
+public:
+    int dfs1(vector<vector<int>>& adj, vector<int>& values, vector<bool>& vist, int node, int k) {
+    vist[node] = true;
+    int cmp_sum = values[node];
 
-typedef pair<int, int> pii;
-typedef vector<int> vi;
-typedef vector<pii> vp;
-typedef vector<vi> vvi;
-int lcm(int a, int b)
-{
-    return (a / __gcd(a, b)) * b;
-}
-
-bool clean(string str)
-{
-    bool flag = false;
-    int n;
-
-    for (int i = 0; i < n - 1; i++)
-    {
-        if (str[i] == 'A' && str[i + 1] == 'B')
-        {
-            flag = true;
-        }
-        else if (str[i] == 'B' && str[i + 1] == 'B')
-        {
-            flag = true;
-        }
-    }
-    return flag;
-}
-
-int check(int i, vector<int> ans)
-{
-    for (auto it : ans)
-    {
-        if (it % i != 0)
-        {
-            return -1;
+    for (int neighbor : adj[node]) {
+        if (!vist[neighbor]) {
+            cmp_sum += dfs(adj, values, vist, neighbor, k);
         }
     }
 
-    if (i == 1)
-        return 2;
-
-    for (int index = 2; index < i; index++)
-    {
-        if (i % index != 0)
-        {
-            return index;
-        }
+    if (cmp_sum % k == 0) {
+        return 0;
+    } else {
+        return cmp_sum;
     }
-
-    return i + 1;
 }
 
-char get(int i)
-{
-    return 'a' + i - 1;
-}
-
-const int INF = 1e9 + 7;
-
-int myans(int n)
-{
-    int pow = 1;
-    while (pow <= n)
-    {
-        pow *= 2;
-    }
-    return pow;
-}
-
-int bpow(int base, int exp)
-{
-    int res = 1;
-    while (exp)
-    {
-        if (exp % 2 == 1)
-        {
-            res = (res * base) % mod;
-        }
-        exp >>= 1;
-        base = (base * base) % mod;
-    }
-    return res;
-}
-
-int badd(int a, int b)
-{
-    return (a + b) % mod;
-}
-int bsub(int a, int b)
-{
-    return (((a - b) % mod) + mod) % mod;
-}
-int bmul(int a, int b)
-{
-    return ((a % mod) * (b % mod)) % mod;
-}
-
-int binv(int base)
-{
-    return bpow(base, mod - 2);
-}
-
-int bdiv(int a, int b)
-{
-    return bmul(a, binv(b));
-}
-
-// function to find factorial of a n number
-int fact(int n)
-{
-    int sum = 1;
-    for (int i = 2; i <= n; i++)
-    {
-        sum *= i;
-    }
-    return sum;
-}
-int maxi = INT_MIN;
-
-void solve()
-{
-    int n;
-    cin>>n;
-
-    vector<int> ans;
-
-    if(n%2!=0)
-    {
-        for(int i=1; i<n/2+1 ;i++)
-        {
-            ans.push_back(3*i);
-            ans.push_back(3*i+1);
-        }
-        ans.push_back(3*(n/2)+3);
+    int maxKDivisibleComponents(int n, vector<vector<int>>& edges, vector<int>& values, int k) {
         
-    }
-    else
-    {
-        for(int i=1; i<n/2+1 ;i++)
-        {
-            ans.push_back(3*i);
-            ans.push_back(3*i+1);
+        vector<vector<int>> adj(n);
+        for (const vector<int>& edge : edges) {
+            int u = edge[0], v = edge[1];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
         }
         
+        vector<bool> vist(n, false);
+        int mx_cmp = 0;
+        
+      
+        function<int(int)> dfs = [&](int node) {
+            vist[node] = true;
+            int cmp_sum = values[node];
+            
+            for (int neighbor : adj[node]) {
+                if (!vist[neighbor]) {
+                    cmp_sum += dfs(neighbor);
+                }
+            }
+            
+            if (cmp_sum % k == 0) {
+                mx_cmp++;
+                return 0;
+            } else {
+                return cmp_sum; 
+            }
+        };
+        
+        for (int i = 0; i < n; ++i) {
+            if (!vist[i]) {
+               
+                int cmp_sum = dfs(i);
+                if (cmp_sum != 0) {
+                    mx_cmp++;
+                }
+            }
+        }
+        
+        return mx_cmp;
     }
-
-    
-
-    for(int i=0;i<n;i++){
-        cout<<ans[i]<<" ";
-    }
-    cout<<endl;
-}
-
-int32_t main()
-{
-
-    fastIO;
-    auto start = std::chrono::high_resolution_clock::now();
-
-    int t;
-    cin>>t;
-    while (t--)
-        solve();
-
-    auto stop = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start);
-    // #ifndef ONLINE_JUDGE
-    //     cerr << "Time taken : " << ((long double)duration.count())/((long double) 1e9) <<"str"<< endl;
-    // #endif
-    return 0;
-}
+};
